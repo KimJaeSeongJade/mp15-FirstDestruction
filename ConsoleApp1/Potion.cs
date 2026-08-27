e9a880a75833c915cb93fd617203bb898e3736cc
baewhv <51593013+baewhv@users.noreply.github.com>
2026-08-27 15:17:57 +0900

    [FEAT] : 포션 생성자 적용

--- ConsoleApp1/Potion.cs
+++ ConsoleApp1/Potion.cs
@@ -1,7 +1,14 @@
 ﻿public class Potion : Item
 {
+    public int HealValue { get;private set; }
+
+    public Potion(int healValue)
+    {
+        HealValue = healValue;
+    }
+    
     public override void Interact(Monster monster)
     {
-        monster.Health += 20;
+        monster.Health += HealValue;
     }
 }
