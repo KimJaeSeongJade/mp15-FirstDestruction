11501b09d0478b72d1613b1b59b0f2fd2781fcfc
baewhv <51593013+baewhv@users.noreply.github.com>
2026-08-27 15:02:08 +0900

    [FEAT] : 몬스터볼 추가

---
+++ ConsoleApp1/Ball.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1;
+
+public class Ball : Item
+{
+    public float CatchRatio{get; private set;}
+
+    public Ball(float catchRatio)
+    {
+        CatchRatio = catchRatio;
+    }
+
+    public override void Interact(Monster monster)
+    {
+        
+        
+    }
+}
